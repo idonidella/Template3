687cc915d709525b8050e5c9d7c5beb9849dc580
Efekan Efe <efekanefe63186@gmail.com>
2023-04-16 23:47:49 +0300

    Template3

---
+++ Template3/js/template3.js
@@ -0,0 +1,152 @@
+$('.main-slider').owlCarousel({
+    loop:true,
+    margin:0,
+    nav:false,
+    dots: false,
+    items:1,
+    autoplay:true,
+    autoplayTimeout:4000,
+    autoplayHoverPause:true,
+  
+
+})
+
+$('.center-slider').owlCarousel({
+    loop:true,
+    nav:false,
+    autoplay:true,
+    autoplayTimeout:2000,
+    autoplayHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:4
+        },
+
+        2000:{
+            items:5
+        }
+
+
+    }
+
+})
+
+
+$('.video-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:2
+        },
+        1000:{
+            items:2
+        }
+    }
+
+})
+
+
+
+$('.videobottom-slider').owlCarousel({
+    loop:true,
+    margin:10,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:3000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+
+})
+
+
+
+
+$('.bigcenter-slider').owlCarousel({
+    loop:true,
+    margin:5,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:1
+        },
+        1000:{
+            items:1
+        }   
+    }
+
+})
+
+
+
+$('.bigcenter2-slider').owlCarousel({
+    loop:true,
+    margin:20,
+    nav:false,
+    dots:false,
+    autoplay:true,
+    autoplayTimeout:5000,
+    autoplyHoverPause:true,
+     responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:1
+        },
+        800:{
+            items:3
+        },
+        1000:{
+            items:3
+        }
+    }
+
+})
+
+
+
